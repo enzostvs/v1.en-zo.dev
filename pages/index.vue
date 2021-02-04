ea0fce28ebcb935a7282cc7c8b9b8c9501476908
Enzo Esteves <eesteves@hotmail.fr>
2021-02-04 14:27:35 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <img :src="require(`~/assets/images/avatar_${random}.svg`)" class="w-16" alt="Memoji Enzo">
       </div>
       <p class="text-2xl font-medium mt-10 mb-5">Hi, I'm Enzo&nbsp;👋</p>
-      <p class="text-medium text-gray-600">Work In Progress</p>
+      <p class="text-medium text-gray-600">Site under construction...</p>
       <h1 class="text-6xl font-bold leading-tight">
         Front-End
         <span class="bg-clip-text	bg-gradient-to-r from-indigo-500 via-purple-400 to-indigo-500 text-fill-transparent">Developer</span>
